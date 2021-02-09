26580abb795dc51c4b1745500c6c3280e12921dd
Alan Christian <alanchristiancreator@hotmail.com>
2021-02-09 20:47:37 -0300

    Logo structure

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { HeaderArea } from './styled';
+
+import logo from '../../../assets/logo.svg';
 
 const Header = () => {
   return (
     <HeaderArea>
-      ...
+      <div className='container'>
+        <div className='logo'>
+          <Link to='/'>
+            <img className='logo' src={logo} alt='logo' />
+          </Link>
+        </div>
+      </div>
     </HeaderArea>
   );
 }
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-  height: 100px;
-  background-color: #000;
+  background-color: #fff;
+  height: 80px;
+  border-bottom: 1px solid #ccc;
+  
+  .container {
+    max-width: 1272px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    padding-top: 15px;
+  }
 
+  .logo {
+  display: flex;
+  align-items: center;
+
+  border: 1px solid #D7DBDD;
+  width: 48px;
+  height: 48px;
+  border-radius: 100%;
+  box-shadow:
+    0 2px 1px rgba(0, 0, 0, 0.05),
+    0 2px 3px rgba(0, 0, 0, 0.05),
+    0 4px 8px rgba(0, 0, 0, 0.05);
+  }
 `;
