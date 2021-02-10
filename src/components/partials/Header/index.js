f00f861dfc411963552ad74e5c7f1e123f0237c9
Alan Christian <alanchristiancreator@hotmail.com>
2021-02-10 20:31:33 -0300

    AuthHandler

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { HeaderArea } from './styled';
 
+import { isLogged } from '../../../helpers/AuthHandler';
+
+import { HeaderArea } from './styled';
 import logo from '../../../assets/logo.svg';
 
 const Header = () => {
+  let logged = isLogged();
+
   return (
     <HeaderArea>
       <div className='container'>
@@ -15,15 +19,33 @@ const Header = () => {
         </div>
         <nav>
           <ul>
-            <li>
-              <Link to=''>Login</Link>
-            </li>
-            <li>
-              <Link className='' to=''>Cadastrar</Link>
-            </li>
-            <li>
-              <Link className='btn-cta' to=''>Poste um anúncio</Link>
-            </li>
+            {logged && 
+              <>
+                <li>
+                  <Link to='/my-account'>Minha Conta</Link>
+                </li>
+                <li>
+                  <Link to='/logout'>Sair</Link>
+                </li>
+                <li>
+                  <Link className='btn-cta' to='/post-an-ad'>Anunciar</Link>
+                </li>
+              </>
+            }
+            {!logged &&
+              <>
+                <li>
+                  <Link to='/signin'>Login</Link>
+                </li>
+                <li>
+                  <Link to='/signup'>Cadastrar</Link>
+                </li>
+                <li>
+                  <Link className='btn-cta' to='/signin'>Anunciar</Link>
+                </li>
+              </>
+            }
+            
           </ul>
         </nav>
       </div>
