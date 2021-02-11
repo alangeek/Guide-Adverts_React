00b20ef62de8435b0ac6b8ddc5646d57869af186
Alan Christian <alanchristiancreator@hotmail.com>
2021-02-11 20:07:34 -0300

    Initial signin

--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 export const Template = styled.div``;
 
-export const PageContainer = styled.div``;
+export const PageContainer = styled.div`
+  max-width: 1000px;
+  margin: auto;
+`;
 
 export const PageTitle = styled.h1``;
 
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { PageArea } from './styled';
+import { PageContainer, PageTitle } from '../../components/MainComponents';
+
+const Page = () => {
+  return (
+    <PageContainer>
+      
+    </PageContainer>
+  );
+}
+
+export default Page;
